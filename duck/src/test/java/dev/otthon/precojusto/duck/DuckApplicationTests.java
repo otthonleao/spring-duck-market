d6abb8f76cdff79da3e00601387576376cb7ed26
Otthon Leão <otthonleao@hotmail.com>
2025-01-31 03:11:43 -0400

    chore: add modulo duck

---
+++ duck/src/test/java/dev/otthon/precojusto/duck/DuckApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otthon.precojusto.duck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
